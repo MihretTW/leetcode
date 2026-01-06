235c125bb2dfe1a78f610486099a37c27991d1d0
Mihret Tadesse <mercytadesse40@gmail.com>
2026-01-06 14:18:40 +0300

    Time: 19 ms (28.83%) | Memory: 19.3 MB (12.71%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.py
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.py
@@ -1,8 +1,8 @@
 class Solution:
     def pivotInteger(self, n: int) -> int:
         for i in range(n,-1,-1):
-            x=sum(range(1,i+1))
-            y=sum(range(i,n+1))
+            x=(i*(i+1))//2
+            y=((n-i+1)*(n+i))//2
             if x==y:
                 return i
         return -1
