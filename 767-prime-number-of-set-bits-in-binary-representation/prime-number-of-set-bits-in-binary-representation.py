c0e555b873ea1836f89721762f382d599748d34a
Mihret Tadesse <mercytadesse40@gmail.com>
2026-02-21 21:29:51 +0300

    Time: 74 ms (66.10%) | Memory: 19.5 MB (40.29%) - LeetSync

--- 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.py
+++ 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.py
@@ -3,7 +3,7 @@ class Solution:
         prime=[ 2, 3, 5, 7, 11, 13, 17,19]
         count=0
         for i in range(left,right+1):
-            x=str(bin(i))
+            x=bin(i)
             y=x.count('1')
             if y in prime:
                 count+=1
