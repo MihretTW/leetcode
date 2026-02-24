97a0206734517b6ee9fc68b14e271767b5852716
Mihret Tadesse <mercytadesse40@gmail.com>
2026-02-24 10:02:56 +0300

    Time: 66 ms (21.87%) | Memory: 19.2 MB (79.63%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -2,12 +2,16 @@ class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         left=0
         right=len(s1)
-        s1=sorted(s1)
-        while left<right<=len(s2):
-            if sorted(s2[left:right])==s1:
-                return True
-            else:
+        count=Counter(s1)
+        window_count=Counter()
+        for right in range(len(s2)):
+            window_count[s2[right]]+=1
+            if right-left+1>len(s1):
+                window_count[s2[left]]-=1
+                if window_count[s2[left]]==0:
+                    del window_count[s2[left]]
                 left+=1
-                right+=1
+            if window_count==count:
+                return True
         return False
         
